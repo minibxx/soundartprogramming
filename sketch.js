bfb44c9372e41d387ae10533ae00050b3138503b
minibxx <101965472+minibxx@users.noreply.github.com>
2022-12-17 07:10:50 +0000

    초기세팅

--- sketch.js
+++ sketch.js
@@ -1,27 +1,26 @@
 var wave;
-var playing = false;
+
 var button;
 function setup(){
   createCanvas(720,256);
   wave = new p3.Oscillator();
   wave.setType('sine');
-  //wave.start(); #소리 나기 시작
-  wave.freq(440); #주파수
-  wave.amp(0); #볼륨
+  wave.start();
+  wave.freq(440);
+  wave.amp(0);
 
   button = createButton('play/pause');
   button.mousePressed(toggle);
 }
-  function draw(){
+function draw(){
+
 }
-  function toggle(){
-    if(!playing){
-      wave.start();
-      wave.amp(0.5,1);
-      playing= true;
-    }else{
-      wave.ammp(0,1);
-      playing = false;
-    }
-    }
+function toggle(){
+  if(!playing){
+    wave.amp(0.5,1);
+    playing= true;
+  }else{
+    wave.ammp(0,1);
+    playing = false;
+  }
 }
