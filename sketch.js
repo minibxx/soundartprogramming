6d1a20a0a1c418d94b21b93ed2561e31951c1baa
minibxx <101965472+minibxx@users.noreply.github.com>
2022-12-17 07:13:52 +0000

    오류수정

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var wave;
 var button;
 function setup(){
   createCanvas(720,256);
-  wave = new p3.Oscillator();
+  wave = new p5.Oscillator();
   wave.setType('sine');
   wave.start();
   wave.freq(440);
