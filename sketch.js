d2be835485bcf10fb5dece467a0f499fa5884037
minibxx <101965472+minibxx@users.noreply.github.com>
2022-12-17 13:14:48 +0000

    ios-test

--- sketch.js
+++ sketch.js
@@ -1,25 +1,17 @@
-var wave;
-var button;
-var playing;
+let osc;
 
 function setup() {
-createCanvas(720, 256);
-wave = new p5.Oscillator();
-wave.setType('sine');
-wave.start();
-wave.freq(440);
-wave.amp(0);
-button = createButton('play/pause');
-button.mousePressed(toggle);
+  osc = new p5.Oscillator("sine");
+
+  osc.amp(0.5); //소리의 크기(0과 1 사이의 값으로 소수점으로 처리)
+  osc.freq(440);//주파수 값 설정(수업자료 참고)
 }
-function draw(){
+
+function draw() {
+  
 }
-function toggle() {
-if (!playing) {
-wave.amp(0.5, 1);
-playing = true;
-} else {
-wave.amp(0, 1);
-playing = false;
-}
+
+function touchStarted() { //터치 허용 함수(p5js 문법)
+  getAudioContext().resume(); //강제로 사운드 허용하는 문법(구글 및 애플 뚫기)
+  osc.start();//오실레이터 시작
 }
